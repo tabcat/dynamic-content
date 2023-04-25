62673fb320258dd5baa1518f37a292ba28f750ef
tabcat <tabcat00@proton.me>
2023-04-25 08:49:12 -0500

    dynamic-content.ts more readable

--- src/dynamic-content.ts
+++ src/dynamic-content.ts
@@ -4,22 +4,28 @@ import * as codec from '@ipld/dag-cbor'
 import { sha256 as hasher } from 'multiformats/hashes/sha2'
 import type { BlockView } from 'multiformats/block/interface'
 
-export async function DynamicContent
-  ({ protocol, param }: { protocol: string, param: any }):
-  Promise<{ id: CID, block: BlockView }>
+// takes description of the dynamic content (protocol + params)
+// returns manifest (Block) and dynamic-content id (CID)
+export async function DynamicContent (
+  { protocol, param }: { protocol: string, param: any }
+):
+  Promise<{ id: CID, manifest: BlockView }>
 {
-  const block = await Block.encode({ value: { protocol, param }, codec, hasher })
-  const dynamic = new TextEncoder().encode('dynamic')
-  const bytes = new Uint8Array(dynamic.length + block.cid.multihash.digest.length)
-  bytes.set(dynamic)
-  bytes.set(block.cid.multihash.digest, dynamic.length)
 
+  // create manifest
+  const manifest = await Block.encode({ value: { protocol, param }, codec, hasher })
+
+  // create dcid
+  const dynamic = new TextEncoder().encode('dynamic')
+  const bytes = new Uint8Array(dynamic.length + manifest.cid.multihash.digest.length)
+  bytes.set(dynamic)
+  bytes.set(manifest.cid.multihash.digest, dynamic.length)
   const cid = CID.create(
-    block.cid.version,
-    block.cid.code,
+    manifest.cid.version,
+    manifest.cid.code,
     await hasher.digest(bytes)
   )
 
-  return { id: cid, block }
+  return { id: cid, manifest }
 }
 
